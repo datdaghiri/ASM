100c7aad7edd29e70fc705ed13979fe6425d9fc6
datdaghiri <saiminhtetthar@gmail.com>
2025-08-04 02:13:23 +0630

    Create layout.tsx

---
+++ src/app/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type {Metadata} from 'next';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster"
+
+export const metadata: Metadata = {
+  title: 'ASM Kindergarten',
+  description: 'An app that show letters and their sounds',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased bg-background">
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
